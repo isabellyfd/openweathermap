bb345cd85481b22960a8307a9e2aeae5c87f2d8c
Isabelly <icfd@cin.ufpe.br>
2017-01-13 00:36:37 -0300

    Added InLoco sdk

---
+++ openweather/InlocoAdsHandler.swift
@@ -0,0 +1,9 @@
+//
+//  InlocoAdsHandler.swift
+//  openweather
+//
+//  Created by Isabelly Damascena on 13/01/17.
+//  Copyright © 2017 Isabelly Damascena. All rights reserved.
+//
+
+import Foundation
